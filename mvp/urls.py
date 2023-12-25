da590118bdd5433f1ca67a674c60018b756f16c4
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-12-25 13:42:05 +0300

    added urls for swagger

--- mvp/urls.py
+++ mvp/urls.py
@@ -15,9 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from drf_yasg.openapi import Info, Contact, License
+from rest_framework.permissions import AllowAny
+schema_view = get_schema_view(
+    Info(
+        title='Eventforme',
+        default_version='1.0',
+        description='full documentation api',
+        contact=Contact(email='ogannes@mail.ru'),
+        license=License(name='MIT')
+    ),
+    public=True,
+    permission_classes=(AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users.urls')),
+    path('swagger/', schema_view.with_ui()),
 
 ]
